5f510a83d68ace124c9e32dee6e0960541a24ea6
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2023-05-14 23:15:47 -0300

    Instanciando arrays

--- ShoppingList/src/ShoppingList.java
+++ ShoppingList/src/ShoppingList.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingList {
+	
+	public static List<String> itens = new ArrayList<String>();
+	public static List<Integer> amounts = new ArrayList<Integer>();
 	
 	public static void main(String[] args) {
 		
