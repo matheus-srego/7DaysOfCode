299a8bbbd5c5ff6c27f450c700987e0da8ecc505
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2023-05-15 23:36:43 -0300

    Creating methods for insert and test insert

--- ShoppingList/src/ShoppingList.java
+++ ShoppingList/src/ShoppingList.java
@@ -6,7 +6,44 @@ public class ShoppingList {
 	public static List<String> itens = new ArrayList<String>();
 	public static List<Integer> amounts = new ArrayList<Integer>();
 	
-	public static void main(String[] args) {
+	public static void main( String[] args ) {
+		
+		itensInsertionTest();
+		
+	}
+	
+	public static void insertItens( String itemName, Integer itemQuantity ) {
+		
+		itens.add( itemName );
+		amounts.add( itemQuantity );
+		
+	}
+	
+	/* --- TEST METHODS --- */
+	public static void itensInsertionTest() {
+		
+		List<String> itensTest = new ArrayList<String>(
+			List.of( "Feijão", "Arroz", "Biscoito", "Banana", "Maça" )
+		);
+		
+		List<Integer> amountsTest = new ArrayList<Integer>(
+			List.of( 2, 2, 3, 10, 5 )
+		);
+		
+		for( int i = 0; i < itensTest.size(); i++ ) {
+			for( int j = 0; j <= amountsTest.size(); j++ ) {
+				
+				if( i == j ) {
+					
+					String item = itensTest.get( i );
+					Integer amount = amountsTest.get( j );
+					
+					insertItens( item, amount );
+					break;
+					
+				}
+			}
+		}
 		
 	}
 }
